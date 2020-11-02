9c558aebb118a831c53d96446df339303b942cc4
Rina <rinahan4146@gmail.com>
2020-11-02 00:14:15 -0800

    modify props style of Walk buttons

---
+++ comps/WButton/BasicButton/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 4,
+    // flexDirection: "row",
+    alignItems:"center",
+    justifyContent:"center",
+  },
+  text: {
+    color: "#FFF",
+    // fontSize:16
+  },
+});
+
+const BasicButton = ({text, size, backgroundColor, height, width}) => {
+  
+  const fontSize = [{ fontSize: size ? size : 16}] ;
+  const widthstyles = [{ width: width ? width : null} , { height: height ? height : 50}] ;
+  const sizestyles = [styles.container, widthstyles];
+  const fontstyle = [styles.text, fontSize];
+  
+  return (
+      <View 
+      style={sizestyles} 
+      backgroundColor={backgroundColor} 
+      >
+        <Text style={fontstyle}>{text}</Text>
+      </View>
+  );
+};
+
+BasicButton.defaultProps = {
+    // size: null,
+    text: null,
+    backgroundColor: null,
+    // height: null
+};
+
+export default BasicButton;
