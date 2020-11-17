cace9f08f502fd81ecac03798fb0195f6e415e47
Rina <rinahan4146@gmail.com>
2020-11-17 12:29:40 -0800

    fix bugs

--- comps/WalkerProfile/index.js
+++ comps/WalkerProfile/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import styled from "styled-components/native";
-
-import UserInfo from "../UserInfo";
-import Avatar07 from '../../comps/Avatar/Avatar07';
-import ReviewStars from "../ReviewStars";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 import global from "../../Pages/globaj";
-
 
 const MainCont = styled.View`
   width:353px;
@@ -152,52 +147,8 @@ const walkerpicture = require("./selfie.jpg");
 
 
 
-const styles = StyleSheet.create({ 
-  box: {
-    width:353,
-    height:196,
-    backgroundColor:"red",
-    backgroundColor: "#FFFFFF",
-    borderRadius: 26,
-    alignItems:"center",
-    justifyContent:"space-between",
-  },
-  container: {
-    width:"80%",
-    flexDirection:"row",
-    justifyContent:"center",
-  },
-  rightcont: {
-    marginTop:20,
-    height:150,
-    justifyContent:"center",
-    justifyContent:"space-between",
-  },
-});
-
-
 const WalkerProfile = ({city, age, bio, name}) => {
   return (
-//     <View style={styles.box}>
-      
-     
-//     <View style={styles.container}>
-//       <View>
-//         <UserInfo/>
-//         <Text 
-//           style={styles.extratext}
-//           style={{fontSize:10, marginTop:10}}>View full profile</Text>
-//       </View>
-
-//       <View style={styles.rightcont}>
-//       <Avatar07
-//           image1={require('../../comps/Avatar/face3.jpg')}
-//       />
-//       <ReviewStars/>
-//       </View>
-//     </View>
-    
-// </View>
     <View >
       <MainCont style={global.bg}>
         <LeftCont>
