62be5a2af4615e69d1fd97274151ba794381f465
Rina <rinahan4146@gmail.com>
2020-11-21 06:34:09 -0800

    modified WalkerDashPage

--- App.js
+++ App.js
@@ -1,17 +1,79 @@
-import React, { Component } from "react";
-import { View, StyleSheet } from "react-native";
-import Button from "./comps/Button";
+// import React, { Component } from "react";
+// import { View, StyleSheet } from "react-native";
+// import Button from "./comps/Button";
 
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <View>
+//         <Button 
+//         backgroundColor="red"/>
+
+//       </View>
+//   );
+// };
+
+// // export default App;
+// export {default} from './storybook'; 
+
+import React from "react";
+import { View, Text, StyleSheet, ScrollView} from "react-native";
+import DashDog from "./comps/DashDog";
+import DashOwner from "./comps/DashOwner";
+import FooterBar from "./comps/FooterBar";
+import MapPlaceholder from "./comps/MapPlaceholder";
+import TopBar from './comps/TopBar';
+// import AvatarViewProfile from '../AvatarForm/AvatarViewProfile';
+
+const styles = StyleSheet.create({
+  app: {
+    justifyContent:"center",
+    // alignItems:"center",
+    // width:"100%",
+    height:"100%"
+  },
+  container:{
+    alignItems:"center",
+    marginTop:40,
+  
+  },
+  map: {
+    
+  },
+  dashcont: {
+    flexDirection: "row",
+    width: 360,
+    justifyContent:"space-between",
+    marginTop:40
+  },
+  
+});
+
+const WalkerDashPage = ({}) => {
+ 
   return (
-    <View>
-        <Button 
-        backgroundColor="red"/>
-
+      <View style={styles.app}>
+        <ScrollView>
+            <TopBar title="Dashboard" />
+          <View style={styles.container}>
+            <MapPlaceholder style={styles.map}/>
+            <View style={styles.dashcont}>
+              <DashDog/>
+              <DashOwner/>
+            </View>
+          </View>
+        </ScrollView>
+        <FooterBar/>
       </View>
   );
 };
 
-// export default App;
-export {default} from './storybook'; 
+WalkerDashPage.defaultProps = {
+    // size: null,
+    text: null,
+    backgroundColor: null,
+    // height: null
+};
+
+
+export default WalkerDashPage;
--- Pages/OwnerDashpage/index.js
+++ Pages/OwnerDashpage/index.js
@@ -1,104 +1,60 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import styled from "styled-components/native";
-import FooterBar from "./Comps/FooterBar";
-import WalkerProfile from "./Comps/WalkerProfile";
-import ReviewStars from "./Comps/ReviewStars";
-import WalkerProfileTitle from "./Comps/WalkerProfileTitle";
-import TopBar from "./Comps/TopBar";
-import MapPlaceholder from "./Comps/MapPlaceholder";
+import { View, Text, StyleSheet, ScrollView} from "react-native";
+import DashDog from "./comps/DashDog";
+import DashOwner from "./comps/DashOwner";
+import FooterBar from "./comps/FooterBar";
+import MapPlaceholder from "./comps/MapPlaceholder";
+import TopBar from './comps/TopBar';
+// import AvatarViewProfile from '../AvatarForm/AvatarViewProfile';
 
-const LocationCont = styled.View`
-  /* position: relative; */
-  top: 5%;
-`;
+const styles = StyleSheet.create({
+  app: {
+    justifyContent:"center",
+    // alignItems:"center",
+    // width:"100%",
+    height:"100%"
+  },
+  container:{
+    alignItems:"center",
+    marginTop:40,
+  
+  },
+  map: {
+    
+  },
+  dashcont: {
+    flexDirection: "row",
+    width: 360,
+    justifyContent:"space-between",
+    marginTop:40
+  },
+  
+});
 
-const LocationText = styled.View`
-  margin-left: 50px;
-  margin-bottom: 10px;
-`;
-
-const WalkerTitleCont = styled.View`
-  margin-left: 50px;
-  margin-bottom: 10px;
-`;
-
-const ReviewStarsCont = styled.View`
-  bottom: 17%;
-  margin-left: 200px;
-`;
-const WalkerProfileCont = styled.View`
-  /* position: relative; */
-  top: 20%;
-`;
-
-const Header = styled.View``;
-
-const Main = styled.View`
-  width: 375px;
-  height: 812px;
-  display: flex;
-  flex-direction: column;
-  /* background-color: red; */
-  align-items: center;
-`;
-
-const MainCont = styled.View`
-  width: 100%;
-  height: 812px;
-`;
-
-const FooterCont = styled.View`
-  width: 375px;
-  position: absolute;
-  bottom: 0;
-`;
-
-const Cont = styled.View`
-  width: 100%;
-  height: 86%;
-  /* background-color: blue; */
-  overflow-y: scroll;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const OwnerDashPage = () => {
+const OwnerDashPage = ({}) => {
+ 
   return (
-    <View>
-      <Main>
-        <MainCont>
-          <Header>
+      <View style={styles.app}>
+        <ScrollView>
             <TopBar title="Dashboard" />
-          </Header>
-          <Cont>
-            <LocationCont>
-              <LocationText>
-                <WalkerProfileTitle text="Walker Location" />
-              </LocationText>
-              <MapPlaceholder />
-            </LocationCont>
-            <WalkerProfileCont>
-              <WalkerTitleCont>
-                <WalkerProfileTitle />
-              </WalkerTitleCont>
-              <WalkerProfile
-                age="24"
-                bio="Hi there! My name is Ethan and I love all animals. Dogs, cats, birds, you name it. I really love spending time outdoors in nature and ..."
-              />
-              <ReviewStarsCont>
-                <ReviewStars />
-              </ReviewStarsCont>
-            </WalkerProfileCont>
-          </Cont>
-          <FooterCont>
-            <FooterBar />
-          </FooterCont>
-        </MainCont>
-      </Main>
-    </View>
+          <View style={styles.container}>
+            <MapPlaceholder style={styles.map}/>
+            <View style={styles.dashcont}>
+              <DashDog/>
+              <DashOwner/>
+            </View>
+          </View>
+        </ScrollView>
+        <FooterBar/>
+      </View>
   );
+};
+
+OwnerDashPage.defaultProps = {
+    // size: null,
+    text: null,
+    backgroundColor: null,
+    // height: null
 };
 
 export default OwnerDashPage;
